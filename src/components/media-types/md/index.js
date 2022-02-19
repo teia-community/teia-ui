099b0cec3fa8897274419f422ea5e674314ec5da
Kyle Grover <drakaria@gmail.com>
2022-02-19 10:20:21 -0800

    fix markdown loading (#6)
    switch from async component to useState
    addresses #3

--- src/components/media-types/md/index.js
+++ src/components/media-types/md/index.js
@@ -1,20 +1,30 @@
+import React from 'react';
 import Markdown from 'markdown-to-jsx';
 import { Container, Padding } from '../../layout';
 const axios = require('axios')
 
-export const MD = async ({
+export const MD = ({
     artifactUri
 }) => {
 
-    let data = await axios.get(`https://cloudflare-ipfs.com/ipfs/${artifactUri.split('//')}`).then(res => res.data)
+    const [content, setContent] = React.useState('')
+
+    React.useEffect(() => {
+        let artifactHash = artifactUri.split('/ipfs/')[1]
+        axios.get(`https://cloudflare-ipfs.com/ipfs/${ artifactHash }`)
+            .then(res => {
+                console.log(res)
+                setContent(res.data)
+            })
+    }, [artifactUri])
 
     return (
         <div>
             <Container>
-                <Padding>
+                <Padding>                    
                 <Markdown>
-                {data}
-            </Markdown>
+                    {content}
+                </Markdown>
                 </Padding>
             </Container>
         </div>
