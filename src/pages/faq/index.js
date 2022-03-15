4042dc733c682da99380dafc40be25163c33e0a0
Simon Kusterer <simon@sope.io>
2022-03-15 22:16:37 +0100

    fixed faq styles

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -22,88 +22,93 @@ export class FAQ extends Component {
       <Page title="faq" large>
         <Container>
           <Padding>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/teia-community/teia-docs/wiki/Getting-Started-with-Tezos">
-                <Primary>
-                  <strong>how do I get tezos/make a wallet?</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/teia-community/teia-docs/wiki/How-to-mint-%F0%9F%8C%BF">
-                <Primary>
-                  <strong>how do I mint?</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/teia-community/teia-docs/wiki/Edit-your-profile">
-                <Primary>
-                  <strong>how do I edit my profile?</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/teia-community/teia-docs/wiki/How-to-swap-%F0%9F%94%83">
-                <Primary>
-                  <strong>how do I add/change the price of my OBJKT?</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/teia-community/teia-docs/wiki/How-to-burn-%F0%9F%94%A5">
-                <Primary>
-                  <strong>how do I burn my OBJKT?</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/teia-community/teia-docs/wiki/How-to-resell-%F0%9F%8F%AA">
-                <Primary>
-                  <strong>how do I resell an OBJKT?</strong>
-                </Primary>
-              </Button>
+            <div className={styles.faq__outer__container}>
+              <h1 className={styles.faq__title}>FAQ</h1>
+              <ul className={styles.faq__container}>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/teia-community/teia-docs/wiki/Getting-Started-with-Tezos">
+                    <Primary>
+                      How do I get tezos/make a wallet?
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/teia-community/teia-docs/wiki/How-to-mint-%F0%9F%8C%BF">
+                    <Primary>
+                      How do I mint?
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/teia-community/teia-docs/wiki/Edit-your-profile">
+                    <Primary>
+                      How do I edit my profile?
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/teia-community/teia-docs/wiki/How-to-swap-%F0%9F%94%83">
+                    <Primary>
+                      How do I add/change the price of my OBJKT?
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/teia-community/teia-docs/wiki/How-to-burn-%F0%9F%94%A5">
+                    <Primary>
+                      How do I burn my OBJKT?
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/teia-community/teia-docs/wiki/How-to-resell-%F0%9F%8F%AA">
+                    <Primary>
+                      How do I resell an OBJKT?
+                    </Primary>
+                  </Button>
+                </li>
+              </ul>
             </div>
           </Padding>
         </Container>
 
         <Container>
           <Padding>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/teia-community/teia-docs/wiki/General">
-                <Primary>
-                  <strong>general faq</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/teia-community/teia-docs/wiki/Troubleshooting">
-                <Primary>
-                  <strong>troubleshooting</strong>
-                </Primary>
-              </Button>
-            </div>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/teia-community/teia-docs/wiki/Tools-made-by-the-community">
-                <Primary>
-                  <strong>useful tools</strong>
-                </Primary>
-              </Button>
+            <div className={styles.faq__outer__container}>
+              <hr className={styles.divider}/>
             </div>
           </Padding>
         </Container>
 
-        {/*         <Container>
+        <Container>
           <Padding>
-            <div className={styles.buttons}>
-              <Button href="https://github.com/teia-community/teia-ui/wiki/pt:Home">
-                <Primary>
-                  <strong>português</strong>
-                </Primary>
-              </Button>
+            <div className={styles.faq__outer__container}>
+              <ul className={styles.faq__container}>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/teia-community/teia-docs/wiki/General">
+                    <Primary>
+                      General FAQ
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/teia-community/teia-docs/wiki/Troubleshooting">
+                    <Primary>
+                      Troubleshooting
+                    </Primary>
+                  </Button>
+                </li>
+                <li className={styles.buttons}>
+                  <Button href="https://github.com/teia-community/teia-docs/wiki/Tools-made-by-the-community">
+                    <Primary>
+                      Useful tools
+                    </Primary>
+                  </Button>
+                </li>
+              </ul>
             </div>
           </Padding>
-        </Container> */}
+        </Container>
       </Page>
     )
   }
