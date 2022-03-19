d1204d056965545932d8844972c34eeea53b7b27
Mel Massadian <melmassadian@gmail.com>
2022-03-19 19:00:18 +0100

    fix: ⚡️ use a unique id if ophash is undefined

--- src/pages/objkt-display/tabs/history.js
+++ src/pages/objkt-display/tabs/history.js
@@ -4,6 +4,7 @@ import { Primary } from '../../../components/button'
 import { walletPreview } from '../../../utils/string'
 import { getTimeAgo } from '../../../utils/time'
 import styles from '../styles.module.scss'
+import { uniqueId } from 'lodash'
 
 const MintedIcon = () => (
   <svg
@@ -17,7 +18,7 @@ const MintedIcon = () => (
     <path
       d="M7.45251 1.685C7.62484 1.15463 8.37516 1.15464 8.54749 1.685L9.66687 5.13009C9.74393 5.36728 9.96496 5.52786 10.2144 5.52786H13.8367C14.3944 5.52786 14.6263 6.24147 14.1751 6.56925L11.2445 8.69843C11.0428 8.84502 10.9583 9.10486 11.0354 9.34205L12.1548 12.7871C12.3271 13.3175 11.7201 13.7585 11.2689 13.4307L8.33837 11.3016C8.1366 11.155 7.8634 11.155 7.66163 11.3016L4.73107 13.4307C4.27991 13.7585 3.67288 13.3175 3.84521 12.7871L4.96458 9.34205C5.04165 9.10486 4.95723 8.84502 4.75546 8.69843L1.8249 6.56925C1.37374 6.24147 1.6056 5.52786 2.16326 5.52786H5.78564C6.03504 5.52786 6.25607 5.36728 6.33313 5.13009L7.45251 1.685Z"
       stroke="black"
-      stroke-width="1.5"
+      strokeWidth="1.5"
     />
   </svg>
 )
@@ -204,7 +205,10 @@ export const History = (token_info) => {
                 )
               } else {
                 return (
-                  <div className={`${styles.history}`} key={`s-${e.ophash}`}>
+                  <div
+                    className={`${styles.history}`}
+                    key={`s-${e.ophash | uniqueId()}`}
+                  >
                     <div className={styles.history__event__container}>
                       <SwapIcon />
                       <a
