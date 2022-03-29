1d37f55a02eb0002f6107d7690701560daa20b08
Mel Massadian <melmassadian@gmail.com>
2022-03-29 20:38:25 +0200

    fix: 🎨 infinite scroll on main feed

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,11 @@
 export function rnd(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+export function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[a[i], a[j]] = [a[j], a[i]]
+  }
+  return a
+}
