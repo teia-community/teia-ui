ebe720126e1cfccfa65c67eb6db8a9b05138c2ac
Mel Massadian <melmassadian@gmail.com>
2022-03-31 13:48:01 +0200

    fix: 🔥 add back the interactive symbol (#47)
    * fix: 🔥 add back the interactive symbol
    Closes #40 
    * feat: 🍱 adds icon caching
    * feat: 🔥 use the icon cache in the history tab
    * fix: ✨ adds IconCache to feeds and friends

---
+++ src/icons/trade.js
@@ -0,0 +1,38 @@
+import withIcon from '@utils/with-icon'
+
+const Trade = `<line
+x1="0.75"
+y1="-0.75"
+x2="9.57625"
+y2="-0.75"
+transform="matrix(0.693707 0.720257 -0.693707 0.720257 1.5155 6.98877)"
+stroke="black"
+strokeWidth="1.5"
+strokeLinecap="round"
+/>
+<path
+d="M2.06618 10.708V6.70315H5.92339"
+stroke="black"
+strokeWidth="1.5"
+strokeLinecap="round"
+strokeLinejoin="round"
+/>
+<line
+x1="0.75"
+y1="-0.75"
+x2="9.57625"
+y2="-0.75"
+transform="matrix(0.693707 0.720257 -0.693707 0.720257 5.92212 2.41162)"
+stroke="black"
+strokeWidth="1.5"
+strokeLinecap="round"
+/>
+<path
+d="M10.2153 10.0547H14.0725V6.04987"
+stroke="black"
+strokeWidth="1.5"
+strokeLinecap="round"
+strokeLinejoin="round"
+/>`
+
+export default withIcon(Trade)
