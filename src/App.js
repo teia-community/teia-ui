f6d28166a00cdec36444fd3b9ba8ac9453e29c04
Simon Kusterer <simon@sope.io>
2022-04-28 10:33:21 +0200

    declare routes directly in App component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import { Routes, Route } from 'react-router-dom'
 import HicetnuncContextProvider from '@context/HicetnuncContext'
 import useSettings from '@hooks/use-settings'
 import { Loading as Preloading } from '@components/loading'
-import { routes } from './routes'
+import Sync from './pages/sync'
+import { About } from './pages/about'
+import { FAQ } from './pages/faq'
+import Display from './pages/display'
+import { Latest, Hdao, Random } from './pages/feeds'
+import { Mint } from './pages/mint'
+import { ObjktDisplay } from './pages/objkt-display'
+import { Collaborate, CollabDisplay } from './pages/collaborate'
+import { Galleries } from './pages/galleries'
+import { GalleryDetail } from './pages/gallery-detail'
+import { Config } from './pages/config'
+import { Search } from './pages/search'
+import { Tags } from './pages/tags'
+import { Friends } from './pages/friends'
+import { Terms } from './pages/terms'
 
 const App = () => {
   const { isLoading } = useSettings()
@@ -15,9 +29,27 @@ const App = () => {
   return (
     <HicetnuncContextProvider>
       <Routes>
-        {routes.map(({ path, component: Comp }) => (
-          <Route path={path} key={path} element={<Comp />} />
-        ))}
+        <Route path="/" element={<Search />} />
+        <Route path="/hdao" element={<Hdao />} />
+        <Route path="/random" element={<Random />} />
+        <Route path="/latest" element={<Latest />} />
+        <Route path="/friends/:id" element={<Friends />} />
+        <Route path="/tz/:id/:collection?" element={<Display />} />
+        <Route path="/kt/:id" element={<CollabDisplay />} />
+        <Route path="/collab/:name" element={<CollabDisplay />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/sync" element={<Sync />} />
+        <Route path="/mint" element={<Mint />} />
+        <Route path="/collaborate/:action?" element={<Collaborate />} />
+        <Route path="/objkt/:id" element={<ObjktDisplay />} />
+        <Route path="/galleries" element={<Galleries />} />
+        <Route path="/gallery/:id" element={<GalleryDetail />} />
+        <Route path="/config" element={<Config />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/tags/:id" element={<Tags />} />
+        <Route path="/:id/:collection?" element={<Display />} />
       </Routes>
     </HicetnuncContextProvider>
   )
