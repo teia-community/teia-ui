425c7660dde8735263124d168d42eaa583fb30b7
Mel Massadian <melmassadian@gmail.com>
2022-08-02 21:04:17 +0200

    feat: ⚡️ wip: replaced calls in minter

--- src/data/ipfs.js
+++ src/data/ipfs.js
@@ -4,13 +4,13 @@ import {
 } from '../constants'
 //import { NFTStorage, File } from 'nft.storage'
 
-const { create } = require('ipfs-http-client')
+// const { create } = require('ipfs-http-client')
 const Buffer = require('buffer').Buffer
 const axios = require('axios')
-const readJsonLines = require('read-json-lines-sync').default
+// const readJsonLines = require('read-json-lines-sync').default
 const { getCoverImagePathFromBuffer } = require('../utils/html')
 
-const infuraUrl = 'https://ipfs.infura.io:5001'
+// const infuraUrl = 'https://ipfs.infura.io:5001'
 //const apiKey = process.env.REACT_APP_IPFS_KEY
 //const storage = new NFTStorage({ token: apiKey })
 
@@ -72,24 +72,27 @@ export const prepareFile = async ({
   contentRating,
   formats,
 }) => {
-  console.log('generateDisplayUri', generateDisplayUri)
-  const ipfs = create(infuraUrl)
+  console.debug('generateDisplayUri', generateDisplayUri)
+  // const ipfs = create(infuraUrl)
 
-  const buffer = file.buffer
-  const hash = await ipfs.add(new Blob([buffer]))
-  console.log(hash)
-  const cid = `ipfs://${hash.path}`
+  // const buffer = file.buffer
+  // const hash = await ipfs.add(new Blob([buffer]))
+
+  const _cid = await uploadFileToIPFSProxy(new Blob([file.buffer]))
+  // console.log(hash)
+  const cid = `ipfs://${_cid}`
 
   if (formats.length > 0) {
     formats[0].uri = cid
-    console.log('file format', formats[0])
+    console.debug('file format', formats[0])
   }
 
   // upload cover image
   let displayUri = ''
   if (generateDisplayUri) {
-    const coverHash = await ipfs.add(new Blob([cover.buffer]))
-    displayUri = `ipfs://${coverHash.path}`
+    // const coverHash = await uploadFileToIPFSProxy(new Blob([cover.buffer]))
+    const coverCid = await uploadFileToIPFSProxy(new Blob([cover.buffer]))
+    displayUri = `ipfs://${coverCid}`
     if (cover && cover.format) {
       const format = JSON.parse(JSON.stringify(cover.format))
       format.uri = displayUri
@@ -102,7 +105,7 @@ export const prepareFile = async ({
   // upload thumbnail image
   let thumbnailUri = IPFS_DEFAULT_THUMBNAIL_URI
   if (generateDisplayUri) {
-    const thumbnailInfo = await ipfs.add(thumbnail.buffer)
+    const thumbnailInfo = await uploadFileToIPFSProxy(thumbnail.buffer)
     thumbnailUri = `ipfs://${thumbnailInfo.path}`
     if (thumbnail && thumbnail.format) {
       const format = JSON.parse(JSON.stringify(thumbnail.format))
@@ -156,12 +159,12 @@ export const prepareDirectory = async ({
   }
 
   // upload cover image
-  const ipfs = create(infuraUrl)
+  // const ipfs = create(infuraUrl)
 
   let displayUri = ''
   if (generateDisplayUri) {
-    const coverInfo = await ipfs.add(cover.buffer)
-    displayUri = `ipfs://${coverInfo.path}`
+    const coverCid = await uploadFileToIPFSProxy(cover.buffer)
+    displayUri = `ipfs://${coverCid}`
     if (cover && cover.format) {
       const format = JSON.parse(JSON.stringify(cover.format))
       format.uri = displayUri
@@ -184,7 +187,7 @@ export const prepareDirectory = async ({
   // upload thumbnail image
   let thumbnailUri = IPFS_DEFAULT_THUMBNAIL_URI
   if (generateDisplayUri) {
-    const thumbnailInfo = await ipfs.add(thumbnail.buffer)
+    const thumbnailInfo = await uploadFileToIPFSProxy(thumbnail.buffer)
     thumbnailUri = `ipfs://${thumbnailInfo.path}`
     if (thumbnail && thumbnail.format) {
       const format = JSON.parse(JSON.stringify(thumbnail.format))
@@ -219,40 +222,43 @@ function not_directory(file) {
 async function uploadFilesToDirectory(files) {
   files = files.filter(not_directory)
 
-  const form = new FormData()
+  // const form = new FormData()
 
-  files.forEach((file) => {
-    form.append('file', file.blob, encodeURIComponent(file.path))
-  })
+  // files.forEach((file) => {
+  //   form.append('file', file.blob, encodeURIComponent(file.path))
+  // })
 
-  const endpoint = `${infuraUrl}/api/v0/add?pin=true&recursive=true&wrap-with-directory=true`
-  const res = await axios.post(endpoint, form, {
-    headers: { 'Content-Type': 'multipart/form-data' },
-  })
+  const directory = await uploadFilesToDirectory(files)
 
-  const data = readJsonLines(res.data)
+  // const endpoint = `${infuraUrl}/api/v0/add?pin=true&recursive=true&wrap-with-directory=true`
+  // const res = await axios.post(endpoint, form, {
+  // headers: { 'Content-Type': 'multipart/form-data' },
+  // })
+
+  // const data = readJsonLines(res.data)
 
   // TODO: Remove this once generateDisplayUri option is gone
   // get cover hash
-  let cover = null
-  const indexFile = files.find((f) => f.path === 'index.html')
-  if (indexFile) {
-    const indexBuffer = await indexFile.blob.arrayBuffer()
-    const coverImagePath = getCoverImagePathFromBuffer(indexBuffer)
 
-    if (coverImagePath) {
-      const coverEntry = data.find((f) => f.Name === coverImagePath)
-      if (coverEntry) {
-        cover = coverEntry.Hash
-      }
-    }
-  }
+  // let cover = null
+  // const indexFile = files.find((f) => f.path === 'index.html')
+  // if (indexFile) {
+  //   const indexBuffer = await indexFile.blob.arrayBuffer()
+  //   const coverImagePath = getCoverImagePathFromBuffer(indexBuffer)
 
-  const rootDir = data.find((e) => e.Name === '')
+  //   if (coverImagePath) {
+  //     const coverEntry = data.find((f) => f.Name === coverImagePath)
+  //     if (coverEntry) {
+  //       cover = coverEntry.Hash
+  //     }
+  //   }
+  // }
 
-  const directory = rootDir.Hash
+  // const rootDir = data.find((e) => e.Name === '')
 
-  return { directory, cover }
+  // const directory = rootDir.Hash
+
+  return { directory }
 }
 
 async function uploadMetadataFile({
@@ -270,7 +276,7 @@ async function uploadMetadataFile({
   contentRating,
   formats,
 }) {
-  const ipfs = create(infuraUrl)
+  // const ipfs = create(infuraUrl)
 
   let metadata = {
     name,
@@ -297,5 +303,5 @@ async function uploadMetadataFile({
     metadata.contentRating = contentRating
   }
 
-  return await ipfs.add(Buffer.from(JSON.stringify(metadata)))
+  return await uploadMetadataToIPFSProxy(Buffer.from(JSON.stringify(metadata)))
 }
