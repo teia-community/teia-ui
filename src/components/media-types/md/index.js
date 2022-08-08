aac2c2e6b358cea95971e9990ecf46ec6fe826dc
Mel Massadian <melmassadian@gmail.com>
2022-08-08 20:03:11 +0200

    fix: ⚡️ markdown preview

--- src/components/media-types/md/index.js
+++ src/components/media-types/md/index.js
@@ -3,18 +3,26 @@ import Markdown from 'markdown-to-jsx'
 import { Container, Padding } from '../../layout'
 const axios = require('axios')
 
-export const MD = ({ artifactUri }) => {
+export const MD = ({ artifactUri, previewUri }) => {
   const [content, setContent] = React.useState('')
 
   React.useEffect(() => {
-    let artifactHash = artifactUri.split('/ipfs/')[1]
-    axios
-      .get(`https://cloudflare-ipfs.com/ipfs/${artifactHash}`)
-      .then((res) => {
-        console.log(res)
-        setContent(res.data)
-      })
-  }, [artifactUri])
+    if (artifactUri) {
+      const artifactHash = artifactUri.split('/ipfs/')[1]
+      axios
+        .get(`https://cloudflare-ipfs.com/ipfs/${artifactHash}`)
+        .then((res) => {
+          setContent(res.data)
+        })
+    } else if (previewUri) {
+      if (previewUri.startsWith('data:text/markdown;base64,')) {
+        const base64_md = previewUri.replace('data:text/markdown;base64,', '')
+        setContent(atob(base64_md))
+      } else {
+        console.error('previewUri is not a base64 encoded markdown', previewUri)
+      }
+    }
+  }, [artifactUri, previewUri])
 
   return (
     <div>
