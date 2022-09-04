d755ea4ee7da8b3b15a30640dcfa7925681044cb
Mel Massadian <melmassadian@gmail.com>
2022-09-04 17:34:00 +0200

    fix: ⚡️ fallback to video for gifs

--- src/components/media-types/image/index.js
+++ src/components/media-types/image/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import styles from './styles.module.scss'
+import { VideoComponent } from '../video'
 
 export const ImageComponent = ({
   artifactUri,
@@ -17,12 +18,29 @@ export const ImageComponent = ({
     src = previewUri
   }
 
-  return displayView ? (
+  const [isVideo, setIsVideo] = useState(false)
+  const onError = (e) => {
+    console.debug(`image from ${artifactUri} is actually a video`)
+    setIsVideo(true)
+  }
+  return isVideo ? (
+    <VideoComponent
+      artifactUri={artifactUri}
+      displayUri={displayUri}
+      previewUri={previewUri}
+      preview={preview}
+      onDetailView={onDetailView}
+      displayView={displayView}
+      inView={!displayView}
+      objktID={objktID}
+    />
+  ) : displayView ? (
     <div className={styles.container}>
       <LazyLoadImage
         className={styles.image}
         src={src}
         alt={`object ${objktID} image`}
+        onError={onError}
       />
     </div>
   ) : (
@@ -32,6 +50,7 @@ export const ImageComponent = ({
           className={styles.style}
           src={src}
           alt={`object ${objktID} image`}
+          onError={onError}
         />
       </div>
     </div>
