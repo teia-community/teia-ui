8fcddb3d0ed6ad5e1839efcfcfa1ce403c06d906
Mel Massadian <mel@melmassadian.com>
2023-01-15 23:52:19 +0100

    feat: 🎨 type the localsettingcontext & add utility functions

--- src/hooks/use-local-settings.js
+++ src/hooks/use-local-settings.js
@@ -1,6 +1,10 @@
 import { LocalSettingsContext } from '@context/LocalSettingsProvider'
 import { useContext } from 'react'
 
+/**
+ *
+ * @returns {import("@types").LocalSettingsContext}
+ */
 export default function useLocalSettings() {
   return useContext(LocalSettingsContext)
 }
