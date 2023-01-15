7e35006978a8c421ab9332fe931e73c704fe5ac5
Mel Massadian <mel@melmassadian>
2023-01-15 23:52:19 +0100

    fix: 🔥 unmount issue
    - remove loadable for the most viewed components

--- src/components/header/events/EventMenu.jsx
+++ src/components/header/events/EventMenu.jsx
@@ -2,12 +2,12 @@ import { DropDown } from '@atoms/dropdown'
 import { useTwemoji } from '@hooks/use-twemoji'
 import EventCard from './EventCard'
 
-export const EventMenu = ({ events }) => {
+export const EventMenu = ({ events, setOpen }) => {
   // the reason we created this wrapper..
   useTwemoji()
 
   return (
-    <DropDown menuID="events" vertical>
+    <DropDown setOpen={setOpen} menuID="events" vertical>
       {events?.map((evt) => {
         return <EventCard event={evt} key={`${evt.title} - ${evt.subtitle}`} />
       })}
