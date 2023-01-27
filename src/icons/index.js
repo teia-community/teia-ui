5f4ccf5bb0d336f0b9b83c77e6e350bb9c5b00cc
Mel Massadian <mel@melmassadian>
2023-01-27 15:10:00 +0100

    fix: 🎨 move icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,3 +1,4 @@
+// SVGs Prepared for ref (instancing icons from one source)
 export { default as BurnIcon } from './burn'
 export { default as GenerativeIcon } from './generative'
 export { default as IndexerStatusIcon } from './indexer_status'
@@ -5,3 +6,10 @@ export { default as MintedIcon } from './minted'
 export { default as SwapIcon } from './swap'
 export { default as TradeIcon } from './trade'
 export { FullScreenEnter, FullScreenExit } from './fullscreen-icon'
+
+// SVGs Basic Components
+export { ReactComponent as ChevronIcon } from './svgs/chevron.svg'
+export { ReactComponent as EventIcon } from './svgs/events.svg'
+export { ReactComponent as FiltersIcon } from './svgs/filters.svg'
+export { ReactComponent as MasonryIcon } from './svgs/masonry.svg'
+export { ReactComponent as SingleViewIcon } from './svgs/single_view.svg'
