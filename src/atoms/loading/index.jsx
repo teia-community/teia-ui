4bc589e941a2959ef67c933c018851ac7756351a
Mel Massadian <mel@melmassadian>
2023-01-29 16:06:04 +0100

    fix: 🎨 loading component

--- src/atoms/loading/index.jsx
+++ src/atoms/loading/index.jsx
@@ -5,19 +5,10 @@ import { memo } from 'react'
 export const Loading = memo(({ message }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.circle} />
-      {message && <p className={styles.message}>{message}</p>}
+      <div className={styles.loader}>
+        <div className={styles.circle} />
+        {message && <p className={styles.message}>{message}</p>}
+      </div>
     </div>
   )
-})
-
-export const LoadingContainer = memo(({ loading, children = null }) => {
-  if (loading) {
-    return (
-      <div className={styles.loader}>
-        <Loading />
-      </div>
-    )
-  }
-  return children
 })
