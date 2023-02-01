16c437c0dceaed91ff77f1b7daf40fb0aff94ef5
Mel Massadian <mel@melmassadian>
2023-02-01 02:43:29 +0100

    fix: 🐛 use navlinks in menu

--- src/components/header/main_menu/MenuItem.jsx
+++ src/components/header/main_menu/MenuItem.jsx
@@ -3,28 +3,24 @@ import { capitalizeFirstLetter } from '@utils/string'
 import styles from '@style'
 import { useContext } from 'react'
 import { TeiaContext } from '@context/TeiaContext'
-import { useNavigate } from 'react-router-dom'
 
 export const MenuItem = ({ label, route, need_sync, className }) => {
   label = label ? label : capitalizeFirstLetter(route)
   const context = useContext(TeiaContext)
-  const navigate = useNavigate()
-  const handleRoute = (path, data) => {
-    context.collapseMenu(true)
-    navigate(path, { state: data })
-  }
+
   return (
     <Button
       className={`${
         need_sync && !context.acc?.address ? styles.disabled : ''
       } ${className ? className : ''} `}
-      onClick={() => {
-        if (need_sync) {
-          handleRoute('/sync', `/${route}`)
-        } else {
-          handleRoute(`/${route}`)
-        }
-      }}
+      to={
+        need_sync
+          ? {
+              pathname: '/sync',
+              data: `/${route}`,
+            }
+          : `/${route}`
+      }
     >
       {label}
     </Button>
