41018bb4651031d506a936bd84caea561445834b
Mel Massadian <mel@melmassadian.com>
2023-02-03 21:03:55 +0100

    fix: 🎨 wip media type fixes
    Mostly pushed to fix the current deploys, still WIP

--- src/components/media-types/pdf/index.jsx
+++ src/components/media-types/pdf/index.jsx
@@ -1,13 +1,12 @@
-import React, { useContext, useState } from 'react'
+import React, { memo, useMemo, useRef, useState } from 'react'
 import styles from '@style'
 import { Document, Page, pdfjs } from 'react-pdf/dist/esm/entry.webpack5'
+import 'react-pdf/dist/esm/Page/TextLayer.css'
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 
 import { ImageComponent } from '../image'
 import { Button } from '@atoms/button'
-import { TeiaContext } from '@context/TeiaContext'
 import { AnimatePresence } from 'framer-motion'
-import { HashToURL } from '@utils'
 // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.min.js'
 const options = {
@@ -19,31 +18,39 @@ const options = {
 /**
  * @param {import("@types").MediaTypeProps} renderOptions - Th options for the media renderer
  */
-export const PdfComponent = ({
+export const PdfComponent = memo(function ({
   artifactUri,
   displayUri,
   previewUri,
   displayView,
   nft,
-}) => {
+}) {
   const [numPages, setNumPages] = useState(null)
   const [pageNumber, setPageNumber] = useState(1)
+  const [renderedPageNumber, setRenderedPageNumber] = useState(null)
   const [loading, setLoading] = useState(displayView)
-  const context = useContext(TeiaContext)
+  const [pageLoading, setPageLoading] = useState(false)
+
+  const [height, setHeight] = useState(null)
+  // const [loading, setLoading] = useState(displayView)
+
+  const container = useRef()
+
+  const file = useMemo(
+    () => (previewUri ? previewUri : artifactUri),
+    [previewUri, artifactUri]
+  )
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages)
-    setLoading(false)
   }
-  function onDocumentLoadError(e) {
-    console.error(e.message)
-    context.showFeedback(`${e.message}
 
-see it on [IPFS](${HashToURL(nft.artifact_uri)})`)
-    setLoading(false)
+  function onDocumentLoadError(e) {
+    throw Error(e.message, { cause: 'PDF Error' })
   }
 
   function changePage(offset) {
+    setPageLoading(true)
     setPageNumber((prevPageNumber) => prevPageNumber + offset)
   }
 
@@ -55,57 +62,89 @@ see it on [IPFS](${HashToURL(nft.artifact_uri)})`)
     changePage(1)
   }
 
-  function onItemClick(item) {
-    setPageNumber(item.pageNumber)
-  }
-  if (!displayView || loading)
-    return (
-      <AnimatePresence>
-        <ImageComponent
-          // key={`img-${nft.token_id}`}
-          artifactUri={displayUri}
-          displayUri={displayUri}
-          previewUri={previewUri}
-          displayView={!displayView}
-          nft={nft}
-        />
-        {loading && (
-          <p
-            key={`loading-${nft.token_id}`}
-            style={{ textAlign: 'center', margin: '1em' }}
-          >
-            Loading PDF...
-          </p>
-        )}
-      </AnimatePresence>
-    )
+  // function onItemClick(item) {
+  //   setPageNumber(item.pageNumber)
+  // }
 
+  const onRender = () => {
+    if (loading) {
+      setLoading(false)
+      setHeight(container.current.clientHeight)
+    }
+    setRenderedPageNumber(pageNumber)
+    setPageLoading(false)
+  }
+
+  const cover = (
+    <AnimatePresence>
+      <ImageComponent
+        // key={`img-${nft.token_id}`}
+        artifactUri={displayUri}
+        displayUri={displayUri}
+        previewUri={previewUri}
+        displayView={!displayView}
+        nft={nft}
+      />
+      {loading && (
+        <p
+          key={`loading-${nft.token_id}`}
+          style={{ textAlign: 'center', margin: '1em' }}
+        >
+          Loading PDF...
+        </p>
+      )}
+    </AnimatePresence>
+  )
+  console.log({
+    pageLoading,
+    pageNumber,
+    renderedPageNumber,
+  })
+  // const loading = renderedPageNumber !== pageNumber
+  if (!displayView) return cover
   return (
-    <div className={styles.container}>
+    <div ref={container} className={styles.container}>
       <Document
-        file={previewUri ? previewUri : artifactUri}
+        file={file}
+        loading={cover}
         onLoadSuccess={onDocumentLoadSuccess}
         onLoadError={onDocumentLoadError}
-        onItemClick={onItemClick}
         title={`PDF object ${nft.token_id}`}
-        className={`${loading && styles.hidden}`}
         options={options}
       >
-        <Page pageNumber={pageNumber} />
+        <Page
+          key={`ren-${renderedPageNumber}`}
+          className={`${styles.previous_page} ${
+            renderedPageNumber === pageNumber ? styles.hidden : ''
+          }`}
+          pageNumber={renderedPageNumber || 1}
+          height={height}
+        />
+        <Page
+          key={pageNumber}
+          className={styles.page}
+          pageNumber={pageNumber}
+          onRenderSuccess={onRender}
+          height={height}
+        />
       </Document>
-      <div className={styles.pdfNav}>
-        <Button disabled={pageNumber <= 1} onClick={previousPage}>
-          Prev «
-        </Button>
-        <p>
-          Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
-        </p>
-        <Button disabled={pageNumber >= numPages} onClick={nextPage}>
-          >» Next
-        </Button>
-      </div>
+      {!loading && (
+        <>
+          <div className={styles.pdfNav}>
+            <Button disabled={pageNumber <= 1} onClick={previousPage}>
+              Prev «
+            </Button>
+            <p>
+              Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+            </p>
+            <Button disabled={pageNumber >= numPages} onClick={nextPage}>
+              >» Next
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   )
-}
+})
 
 export default PdfComponent
