f3d28a4ad33af35dd54d88f41694fd4e15082a4b
melMass <mel@melmassadian.com>
2023-02-06 17:37:39 +0100

    fix: 🎨 show objkt id on single feeds and artist creations

--- src/components/item-info/ItemInfoCompact.jsx
+++ src/components/item-info/ItemInfoCompact.jsx
@@ -1,43 +1,47 @@
 import { Button } from '@atoms/button'
-// import { PATH } from '@constants'
 import { walletPreview } from '@utils/string'
 import { isNumber } from 'lodash'
 import styles from '@style'
 import Editions from './Editions'
 import useLocalSettings from '@hooks/use-local-settings'
 import { TezosIcon } from '@icons'
-// import { PATH } from '@constants'
+import { PATH } from '@constants'
+import { useParams } from 'react-router'
 
+const Price = ({ price }) => (
+  <div className={styles.price_box}>
+    {price}
+    <TezosIcon size={16} />
+  </div>
+)
 /**
  * @param {Object} itemInfoCompactOptions
  * @param {import("@types").NFT} itemInfoCompactOptions.nft
  **/
 const ItemInfoCompact = ({ nft }) => {
-  const { zen /*viewMode*/ } = useLocalSettings()
+  const { zen, viewMode } = useLocalSettings()
+
+  const param = useParams()
+
   const _price = !zen
     ? isNumber(nft.price)
       ? (Number(nft.price) / 1e6).toString()
       : 'X'
     : null
 
-  const price = (
-    <div className={styles.price_box}>
-      {_price}
-      <TezosIcon size={16} />
-    </div>
-  )
-
   return (
     <div className={`${styles.compact_container} ${styles.masonry}`}>
-      {/* <Button
-        alt={`Go to OBJKT ${nft.token_id}`}
-        to={`${PATH.OBJKT}/${nft.token_id}`}
-      >
-        #{nft.token_id}
-      </Button> */}
+      {(viewMode === 'single' || (param['id'] && param['*'] === '')) && (
+        <Button
+          alt={`Go to OBJKT ${nft.token_id}`}
+          to={`${PATH.OBJKT}/${nft.token_id}`}
+        >
+          #{nft.token_id}
+        </Button>
+      )}
       <Button
         className={styles.artist}
-        activeClass={styles.active}
+        activeClass={styles.hide}
         alt={`Go to artist page of token #${nft.token_id}`}
         to={
           nft.artist_profile?.name
@@ -50,7 +54,7 @@ const ItemInfoCompact = ({ nft }) => {
           : walletPreview(nft.artist_address)}
       </Button>
       <Editions className={styles.editions} nft={nft} />
-      {price}
+      <Price price={_price} />
     </div>
   )
 }
