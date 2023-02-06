4b14567c6f376a867056a912acbd90fc3ab0a818
Mel Massadian <mel@melmassadian>
2023-02-06 04:12:53 +0100

    feat: 📝 merge storybook (only what works)
    This sat in a branch, it's non intrusive but quite useful
    to debug issues in isolation, ideally we would just have some
    mock data as SWR isn't working

---
+++ .storybook/TeiaTheme.ts
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Teia',
+  brandUrl: 'https://teia.art',
+  brandImage:
+    'https://raw.githubusercontent.com/teia-community/teia-logos/main/dist/logos/svg/julioglatt_04.svg',
+  brandTarget: '_self',
+})
---
+++ src/stories/Dropdown.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { decorators } from './shared'
+import DropdownButton from '@atoms/dropdown/DropdownButton'
+import { EventIcon } from '@icons/index'
+import { DropDown } from '@atoms/dropdown/index'
+import { sample_events } from '@components/header/sample_events'
+
+const meta: Meta<typeof DropDown> = {
+  title: 'Atoms/Dropdown',
+  component: DropDown,
+  tags: ['autodocs'],
+  argTypes: {},
+
+  decorators: [decorators.theme],
+}
+
+export default meta
+type Story = StoryObj<typeof DropDown>
+
+export const Base: Story = {
+  render: ({}) => (
+    <DropdownButton
+      alt={'example dropdown'}
+      icon={<EventIcon />}
+      menuID="events"
+      label={'A sample dropdown with an Icon'}
+    >
+      {/* <EventMenu events={sample_events} /> */}
+      <DropDown menuID="events" vertical>
+        {sample_events?.map((evt) => (
+          <div>
+            <h3>{evt.title}</h3>
+            <p>{evt.subtitle}</p>
+          </div>
+        ))}
+      </DropDown>
+    </DropdownButton>
+  ),
+}
