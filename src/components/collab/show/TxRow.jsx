8358b6143792d4f8c37c5fdc60a0866afb36dec1
Mel Massadian <melmassadian@gmail.com>
2023-02-18 12:50:11 +0100

    fix: ⚡️ convert to ts

--- src/components/collab/show/TxRow.jsx
+++ src/components/collab/show/TxRow.jsx
@@ -2,21 +2,40 @@ import { Input } from '@atoms/input'
 import styles from '../../collab/index.module.scss'
 import { Button, Secondary } from '@atoms/button'
 import { CloseIcon } from '@icons'
+import { useState } from 'react'
+import { validateAddress } from '@taquito/utils'
+import { useModalStore } from '@context/modalStore'
 
-export const TxRow = ({ tx, onUpdate, onAdd, onRemove }) => {
-  const _update = (key, value) => {
-    const updatedTx = {
-      ...tx,
-      [key]: value,
+export const TxRow = ({ tx, index, onUpdate, onAdd, onRemove }) => {
+  const [amount, setAmount] = useState('')
+  const [destination, setDestination] = useState('')
+
+  const [valid, setValid] = useState(false)
+
+  // TODO(mel): clean
+  const show = useModalStore((st) => st.show)
+
+  // const _update = (key, value) => {
+  //   const updatedTx = {
+  //     ...tx,
+  //     [key]: value,
+  //   }
+  //   onUpdate(updatedTx)
+  //   console.log({ key, value })
+  // }
+
+  const handleAdd = () => {
+    if (validateAddress(destination)) {
+      onAdd({ index, _to: destination, amount })
+    } else {
+      show('Transfer', `Invalid address ${destination}`)
     }
-
-    onUpdate(updatedTx)
   }
 
   // Handle return key
   const _handleKeyPress = (event) => {
     if (event.code === 'Enter') {
-      onAdd()
+      handleAdd()
     }
   }
 
@@ -24,13 +43,14 @@ export const TxRow = ({ tx, onUpdate, onAdd, onRemove }) => {
     <tr className={styles.row}>
       <td style={{ width: '200px' }}>
         <Input
-          type="text"
+          type="number"
           name="transfer-quantity"
           label="Quantity"
           placeholder="OBJKT quantity"
           min={1}
-          value={tx.amount}
-          onChange={(value) => _update('amount', value)}
+          value={amount}
+          onChange={setAmount}
+          //onChange={(value) => _update('amount', value)}
         />
       </td>
       <td>
@@ -38,25 +58,21 @@ export const TxRow = ({ tx, onUpdate, onAdd, onRemove }) => {
           type="text"
           label="Recipient"
           placeholder="to address (tz...)"
-          value={tx.to_}
-          onChange={(value) => _update('to_', value)}
+          value={destination}
+          onChange={setDestination}
           onKeyPress={_handleKeyPress}
         />
       </td>
-
       {onRemove && (
         <td className={styles.actionCell}>
-          <Button onClick={onRemove}>
-            <Secondary>
-              <CloseIcon />
-            </Secondary>
+          <Button fit onClick={onRemove}>
+            <CloseIcon width={16} />
           </Button>
         </td>
       )}
-
-      {!onRemove && tx.to_ && tx.amount && (
-        <td>
-          <Button shadow_box onClick={onAdd}>
+      {!onRemove && destination && amount && (
+        <td className={styles.actionCell} align="center">
+          <Button fit shadow_box onClick={handleAdd}>
             add
           </Button>
         </td>
