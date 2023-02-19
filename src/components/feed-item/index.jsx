5a497ad32b54763270dada36a7eaf9f95dd3c830
Mel Massadian <melmassadian@gmail.com>
2023-02-19 06:33:03 +0100

    test: ✨ token details on hover

--- src/components/feed-item/index.jsx
+++ src/components/feed-item/index.jsx
@@ -6,12 +6,26 @@ import classnames from 'classnames'
 import { useLocalSettings } from '@context/localSettingsStore'
 import { Link } from 'react-router-dom'
 import { shallow } from 'zustand/shallow'
-
+import { useState } from 'react'
+import { AnimatePresence } from 'framer-motion'
+import { motion } from 'framer-motion'
 /**
  * @param {Object} feedOptions - The options for the feed item
  * @param {import("@types").NFT} feedOptions.nft - The nft to render
  * @returns {React.ReactElement} The feed item
  */
+
+const info_variants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+  },
+}
 export const FeedItem = ({ nft }) => {
   const [nsfwFriendly, photosensitiveFriendly] = useLocalSettings(
     (state) => [state.nsfwFriendly, state.photosensitiveFriendly],
@@ -19,6 +33,8 @@ export const FeedItem = ({ nft }) => {
   )
   const zen = useLocalSettings((st) => st.zen)
   const viewMode = useLocalSettings((st) => st.viewMode)
+  // const element = (hovered) => <div>Hover me! {hovered && 'Thanks!'}</div>
+  const [hover, setHover] = useState(false)
 
   const containerClasses = classnames({
     [styles.container]: true,
@@ -28,7 +44,23 @@ export const FeedItem = ({ nft }) => {
   })
 
   return (
-    <div className={containerClasses}>
+    <div
+      onMouseOver={() => setHover(true)}
+      onFocus={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+      onBlur={() => setHover(false)}
+      // onMouseEnter={() => setHover(true)}
+      // onMouseLeave={() => setHover(false)}
+      className={containerClasses}
+    >
+      <AnimatePresence>
+        {hover && (
+          <motion.div {...info_variants} className={styles.hover_details}>
+            <h1>{nft.name}</h1>
+            <p>{nft.description}</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
       {nft.mime_type.startsWith('audio') ? (
         <RenderMediaType nft={nft} />
       ) : (
