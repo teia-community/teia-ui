d404a3e2f7f8268902f269cb96030d1c6c30b855
Mel Massadian <melmassadian@gmail.com>
2023-02-20 02:01:34 +0100

    fix: ⚡️ move header out of routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import useSettings from '@hooks/use-settings'
 import { Loading as Preloading } from '@atoms/loading'
 import { AnimatePresence } from 'framer-motion'
 import { Debug } from '@atoms/debug'
+import { Header } from '@components/header'
 
 const App = () => {
   const { isLoading } = useSettings()
@@ -16,6 +17,7 @@ const App = () => {
       {/* <ScrollToTop /> */}
       <ScrollRestoration getKey={(location, matches) => location.key} />
       <AnimatePresence mode="wait" initial={false}>
+        <Header key="header" />
         <Outlet />
       </AnimatePresence>
     </>
