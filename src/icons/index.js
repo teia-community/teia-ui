1de8f5c161486eff59fc7a1d8ffec7410e1198da
melMass <mel@melmassadian.com>
2023-04-09 21:14:51 +0200

    style: 🎨 update icons (thanks to @tofudog)
    This is the raw svgs, I did not test anything yet

--- src/icons/index.js
+++ src/icons/index.js
@@ -21,6 +21,40 @@ import { ReactComponent as Pilhei01 } from './svgs/rotating_logos/pilhei_01.svg'
 // import { ReactComponent as Somfay02 } from './svgs/rotating_logos/somfay_02.svg'
 import { ReactComponent as Wans01 } from './svgs/rotating_logos/wans_01.svg'
 
+import { ReactComponent as Bayram_01 } from './svgs/rotating_logos/burka-bayram_01.svg'
+import { ReactComponent as Carolyn_01 } from './svgs/rotating_logos/carolyn_01.svg'
+import { ReactComponent as Waadt_02 } from './svgs/rotating_logos/christian-waadt_02.svg'
+import { ReactComponent as Waadt_03 } from './svgs/rotating_logos/christian-waadt_03.svg'
+import { ReactComponent as CRYPTEMES_01 } from './svgs/rotating_logos/CRYPTEMES_01.svg'
+import { ReactComponent as Djkero_01 } from './svgs/rotating_logos/djkero_01.svg'
+import { ReactComponent as Pretorius_01 } from './svgs/rotating_logos/herman-pretorius_01.svg'
+import { ReactComponent as Safa_01 } from './svgs/rotating_logos/kerim-safa_01.svg'
+import { ReactComponent as Joerres_01 } from './svgs/rotating_logos/klaus-joerres_01.svg'
+import { ReactComponent as Mumuthestan_04 } from './svgs/rotating_logos/mumuthestan_04.svg'
+import { ReactComponent as Natived_01 } from './svgs/rotating_logos/natived_01.svg'
+import { ReactComponent as Norulesjustfeels_05 } from './svgs/rotating_logos/norulesjustfeels_05.svg'
+import { ReactComponent as Oviy_01 } from './svgs/rotating_logos/oviy_01.svg'
+import { ReactComponent as Oviy_02 } from './svgs/rotating_logos/oviy_02.svg'
+import { ReactComponent as Qizzio_02 } from './svgs/rotating_logos/qizzio_02.svg'
+import { ReactComponent as Rodicqart_01 } from './svgs/rotating_logos/rodicqart_01.svg'
+import { ReactComponent as Itzcoatl_01 } from './svgs/rotating_logos/santiago-itzcoatl_01.svg'
+import { ReactComponent as Itzcoatl_02 } from './svgs/rotating_logos/santiago-itzcoatl_02.svg'
+import { ReactComponent as Virgini_01 } from './svgs/rotating_logos/simone-virgini_01.svg'
+import { ReactComponent as Linkous_03 } from './svgs/rotating_logos/spencer-linkous_03.svg'
+import { ReactComponent as Varda_01 } from './svgs/rotating_logos/varda_01.svg'
+import { ReactComponent as Varda_02 } from './svgs/rotating_logos/varda_02.svg'
+import { ReactComponent as Varda_03 } from './svgs/rotating_logos/varda_03.svg'
+import { ReactComponent as Yazid_01 } from './svgs/rotating_logos/yazid_01.svg'
+import { ReactComponent as OneLab_01 } from './svgs/rotating_logos/111v1ab_01.svg'
+import { ReactComponent as OneLab_02 } from './svgs/rotating_logos/111v1ab_02.svg'
+import { ReactComponent as Three_01 } from './svgs/rotating_logos/333_01.svg'
+import { ReactComponent as Acidmanhattan_01 } from './svgs/rotating_logos/acidmanhattan_01.svg'
+import { ReactComponent as Acidmanhattan_02 } from './svgs/rotating_logos/acidmanhattan_02.svg'
+import { ReactComponent as AfrizalRizki_01 } from './svgs/rotating_logos/afrizalRizki_01.svg'
+import { ReactComponent as Alterebro_01 } from './svgs/rotating_logos/alterebro_01.svg'
+import { ReactComponent as Alterebro_02 } from './svgs/rotating_logos/alterebro_02.svg'
+import { ReactComponent as Alterebro_03 } from './svgs/rotating_logos/alterebro_03.svg'
+
 export const RotatingLogoSVG = [
   Annekatrinenderlein01,
   Burkabayram01,
@@ -42,6 +76,39 @@ export const RotatingLogoSVG = [
   //-! Somfay01,
   //-! Somfay02,
   Wans01,
+  Bayram_01,
+  Carolyn_01,
+  Waadt_02,
+  Waadt_03,
+  CRYPTEMES_01,
+  Djkero_01,
+  Pretorius_01,
+  Safa_01,
+  Joerres_01,
+  Mumuthestan_04,
+  Natived_01,
+  Norulesjustfeels_05,
+  Oviy_01,
+  Oviy_02,
+  Qizzio_02,
+  Rodicqart_01,
+  Itzcoatl_01,
+  Itzcoatl_02,
+  Virgini_01,
+  Linkous_03,
+  Varda_01,
+  Varda_02,
+  Varda_03,
+  Yazid_01,
+  OneLab_01,
+  OneLab_02,
+  Three_01,
+  Acidmanhattan_01,
+  Acidmanhattan_02,
+  AfrizalRizki_01,
+  Alterebro_01,
+  Alterebro_02,
+  Alterebro_03,
 ]
 
 // SVGs Prepared for ref (instancing icons from one source)
