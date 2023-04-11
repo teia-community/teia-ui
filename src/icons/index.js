e43686e0045e033ca5c5002e1767ec0f7cc67b8d
melMass <mel@melmassadian.com>
2023-04-11 20:38:15 +0200

    style: 🎨 conform svgs
    disabled a few for now:
    2 need new versions: no_rule_just_feels_05 and varda_03
    
    The other commented ones at the top currently have scaling issues
    I need to take a look in a proper graphic software

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,114 +1,103 @@
 // SVGs Rotating logos
 
-import { ReactComponent as Annekatrinenderlein01 } from './svgs/rotating_logos/annekatrinenderlein_01.svg'
-import { ReactComponent as Burkabayram01 } from './svgs/rotating_logos/burkabayram_01.svg'
-import { ReactComponent as Imnot01 } from './svgs/rotating_logos/imnot_01.svg'
-// import { ReactComponent as Julioglatt01 } from './svgs/rotating_logos/julioglatt_01.svg'
-// import { ReactComponent as Julioglatt02 } from './svgs/rotating_logos/julioglatt_02.svg'
-// import { ReactComponent as Julioglatt03 } from './svgs/rotating_logos/julioglatt_03.svg'
-// import { ReactComponent as Julioglatt04 } from './svgs/rotating_logos/julioglatt_04.svg'
-// import { ReactComponent as Julioglatt05 } from './svgs/rotating_logos/julioglatt_05.svg'
-// import { ReactComponent as Julioglatt06 } from './svgs/rotating_logos/julioglatt_06.svg'
-import { ReactComponent as Maes02 } from './svgs/rotating_logos/maes_02.svg'
-// import { ReactComponent as Merchantcoppola01 } from './svgs/rotating_logos/merchantcoppola_01.svg'
-import { ReactComponent as Mumuthestan02 } from './svgs/rotating_logos/mumuthestan_02.svg'
-import { ReactComponent as Mumuthestan03 } from './svgs/rotating_logos/mumuthestan_03.svg'
-import { ReactComponent as Norulesjustfeels07 } from './svgs/rotating_logos/norulesjustfeels_07.svg'
-import { ReactComponent as Pilhei01 } from './svgs/rotating_logos/pilhei_01.svg'
-// import { ReactComponent as Serg_alec_horomox01 } from './svgs/rotating_logos/serg_alec_horomox_01.svg'
-// import { ReactComponent as Slinkous01 } from './svgs/rotating_logos/slinkous_01.svg'
-// import { ReactComponent as Somfay01 } from './svgs/rotating_logos/somfay_01.svg'
-// import { ReactComponent as Somfay02 } from './svgs/rotating_logos/somfay_02.svg'
-import { ReactComponent as Wans01 } from './svgs/rotating_logos/wans_01.svg'
+// import { ReactComponent as Carolyn_01 } from './svgs/rotating_logos/carolyn_01.svg'
+// import { ReactComponent as Djkero_01 } from './svgs/rotating_logos/djkero_01.svg'
+// import { ReactComponent as Julioglatt_01 } from './svgs/rotating_logos/julioglatt_01.svg'
+// import { ReactComponent as Julioglatt_02 } from './svgs/rotating_logos/julioglatt_02.svg'
+// import { ReactComponent as Julioglatt_03 } from './svgs/rotating_logos/julioglatt_03.svg'
+// import { ReactComponent as Julioglatt_04 } from './svgs/rotating_logos/julioglatt_04.svg'
+// import { ReactComponent as Julioglatt_05 } from './svgs/rotating_logos/julioglatt_05.svg'
+// import { ReactComponent as Julioglatt_06 } from './svgs/rotating_logos/julioglatt_06.svg'
+// import { ReactComponent as Merchantcoppola_01 } from './svgs/rotating_logos/merchantcoppola_01.svg'
+// import { ReactComponent as Mumuthestan_04 } from './svgs/rotating_logos/mumuthestan_04.svg'
+// import { ReactComponent as Norulesjustfeels_05 } from './svgs/rotating_logos/norulesjustfeels_05.svg'
+// import { ReactComponent as Safa_01 } from './svgs/rotating_logos/kerim-safa_01.svg'
+// import { ReactComponent as Serg_alec_horomox_01 } from './svgs/rotating_logos/serg_alec_horomox_01.svg'
+// import { ReactComponent as Slinkous_01 } from './svgs/rotating_logos/slinkous_01.svg'
+// import { ReactComponent as Somfay_01 } from './svgs/rotating_logos/somfay_01.svg'
+// import { ReactComponent as Somfay_02 } from './svgs/rotating_logos/somfay_02.svg'
+// import { ReactComponent as Varda_01 } from './svgs/rotating_logos/varda_01.svg'
+// import { ReactComponent as Varda_03 } from './svgs/rotating_logos/varda_03.svg'
+// import { ReactComponent as Waadt_02 } from './svgs/rotating_logos/christian-waadt_02.svg'
+// import { ReactComponent as Yazid_01 } from './svgs/rotating_logos/yazid_01.svg'
 
-import { ReactComponent as Bayram_01 } from './svgs/rotating_logos/burka-bayram_01.svg'
-import { ReactComponent as Carolyn_01 } from './svgs/rotating_logos/carolyn_01.svg'
-import { ReactComponent as Waadt_02 } from './svgs/rotating_logos/christian-waadt_02.svg'
-import { ReactComponent as Waadt_03 } from './svgs/rotating_logos/christian-waadt_03.svg'
-import { ReactComponent as CRYPTEMES_01 } from './svgs/rotating_logos/CRYPTEMES_01.svg'
-import { ReactComponent as Djkero_01 } from './svgs/rotating_logos/djkero_01.svg'
-import { ReactComponent as Pretorius_01 } from './svgs/rotating_logos/herman-pretorius_01.svg'
-import { ReactComponent as Safa_01 } from './svgs/rotating_logos/kerim-safa_01.svg'
-import { ReactComponent as Joerres_01 } from './svgs/rotating_logos/klaus-joerres_01.svg'
-import { ReactComponent as Mumuthestan_04 } from './svgs/rotating_logos/mumuthestan_04.svg'
-import { ReactComponent as Natived_01 } from './svgs/rotating_logos/natived_01.svg'
-import { ReactComponent as Norulesjustfeels_05 } from './svgs/rotating_logos/norulesjustfeels_05.svg'
-import { ReactComponent as Oviy_01 } from './svgs/rotating_logos/oviy_01.svg'
-import { ReactComponent as Oviy_02 } from './svgs/rotating_logos/oviy_02.svg'
-import { ReactComponent as Qizzio_02 } from './svgs/rotating_logos/qizzio_02.svg'
-import { ReactComponent as Rodicqart_01 } from './svgs/rotating_logos/rodicqart_01.svg'
-import { ReactComponent as Itzcoatl_01 } from './svgs/rotating_logos/santiago-itzcoatl_01.svg'
-import { ReactComponent as Itzcoatl_02 } from './svgs/rotating_logos/santiago-itzcoatl_02.svg'
-import { ReactComponent as Virgini_01 } from './svgs/rotating_logos/simone-virgini_01.svg'
-import { ReactComponent as Linkous_03 } from './svgs/rotating_logos/spencer-linkous_03.svg'
-import { ReactComponent as Varda_01 } from './svgs/rotating_logos/varda_01.svg'
-import { ReactComponent as Varda_02 } from './svgs/rotating_logos/varda_02.svg'
-import { ReactComponent as Varda_03 } from './svgs/rotating_logos/varda_03.svg'
-import { ReactComponent as Yazid_01 } from './svgs/rotating_logos/yazid_01.svg'
-import { ReactComponent as OneLab_01 } from './svgs/rotating_logos/111v1ab_01.svg'
-import { ReactComponent as OneLab_02 } from './svgs/rotating_logos/111v1ab_02.svg'
-import { ReactComponent as Three_01 } from './svgs/rotating_logos/333_01.svg'
 import { ReactComponent as Acidmanhattan_01 } from './svgs/rotating_logos/acidmanhattan_01.svg'
 import { ReactComponent as Acidmanhattan_02 } from './svgs/rotating_logos/acidmanhattan_02.svg'
 import { ReactComponent as AfrizalRizki_01 } from './svgs/rotating_logos/afrizalRizki_01.svg'
 import { ReactComponent as Alterebro_01 } from './svgs/rotating_logos/alterebro_01.svg'
 import { ReactComponent as Alterebro_02 } from './svgs/rotating_logos/alterebro_02.svg'
 import { ReactComponent as Alterebro_03 } from './svgs/rotating_logos/alterebro_03.svg'
+import { ReactComponent as Annekatrinenderlein_01 } from './svgs/rotating_logos/annekatrinenderlein_01.svg'
+import { ReactComponent as Burkabayram_01 } from './svgs/rotating_logos/burkabayram_01.svg'
+import { ReactComponent as CRYPTEMES_01 } from './svgs/rotating_logos/CRYPTEMES_01.svg'
+import { ReactComponent as Imnot_01 } from './svgs/rotating_logos/imnot_01.svg'
+import { ReactComponent as Itzcoatl_01 } from './svgs/rotating_logos/santiago-itzcoatl_01.svg'
+import { ReactComponent as Itzcoatl_02 } from './svgs/rotating_logos/santiago-itzcoatl_02.svg'
+import { ReactComponent as Joerres_01 } from './svgs/rotating_logos/klaus-joerres_01.svg'
+import { ReactComponent as Linkous_03 } from './svgs/rotating_logos/spencer-linkous_03.svg'
+import { ReactComponent as Maes_02 } from './svgs/rotating_logos/maes_02.svg'
+import { ReactComponent as Mumuthestan_02 } from './svgs/rotating_logos/mumuthestan_02.svg'
+import { ReactComponent as Mumuthestan_03 } from './svgs/rotating_logos/mumuthestan_03.svg'
+import { ReactComponent as Natived_01 } from './svgs/rotating_logos/natived_01.svg'
+import { ReactComponent as Norulesjustfeels_07 } from './svgs/rotating_logos/norulesjustfeels_07.svg'
+import { ReactComponent as OneLab_01 } from './svgs/rotating_logos/111v1ab_01.svg'
+import { ReactComponent as OneLab_02 } from './svgs/rotating_logos/111v1ab_02.svg'
+import { ReactComponent as Oviy_01 } from './svgs/rotating_logos/oviy_01.svg'
+import { ReactComponent as Oviy_02 } from './svgs/rotating_logos/oviy_02.svg'
+import { ReactComponent as Pilhei_01 } from './svgs/rotating_logos/pilhei_01.svg'
+import { ReactComponent as Pretorius_01 } from './svgs/rotating_logos/herman-pretorius_01.svg'
+import { ReactComponent as Qizzio_02 } from './svgs/rotating_logos/qizzio_02.svg'
+import { ReactComponent as Rodicqart_01 } from './svgs/rotating_logos/rodicqart_01.svg'
+import { ReactComponent as Three_01 } from './svgs/rotating_logos/333_01.svg'
+import { ReactComponent as Varda_02 } from './svgs/rotating_logos/varda_02.svg'
+import { ReactComponent as Virgini_01 } from './svgs/rotating_logos/simone-virgini_01.svg'
+import { ReactComponent as Waadt_03 } from './svgs/rotating_logos/christian-waadt_03.svg'
+import { ReactComponent as Wans_01 } from './svgs/rotating_logos/wans_01.svg'
 
 export const RotatingLogoSVG = [
-  Annekatrinenderlein01,
-  Burkabayram01,
-  Imnot01,
-  // Julioglatt01,
-  // Julioglatt02,
-  // Julioglatt03,
-  // Julioglatt04,
-  // Julioglatt05,
-  //-! Julioglatt06,
-  Maes02,
-  // Merchantcoppola01,
-  Mumuthestan02,
-  Mumuthestan03,
-  Norulesjustfeels07,
-  Pilhei01,
-  // Serg_alec_horomox01,
-  // Slinkous01,
-  //-! Somfay01,
-  //-! Somfay02,
-  Wans01,
-  Bayram_01,
-  Carolyn_01,
-  Waadt_02,
-  Waadt_03,
-  CRYPTEMES_01,
-  Djkero_01,
-  Pretorius_01,
-  Safa_01,
-  Joerres_01,
-  Mumuthestan_04,
-  Natived_01,
-  Norulesjustfeels_05,
-  Oviy_01,
-  Oviy_02,
-  Qizzio_02,
-  Rodicqart_01,
-  Itzcoatl_01,
-  Itzcoatl_02,
-  Virgini_01,
-  Linkous_03,
-  Varda_01,
-  Varda_02,
-  Varda_03,
-  Yazid_01,
-  OneLab_01,
-  OneLab_02,
-  Three_01,
+  // - Scale issues
+  // Mumuthestan_04,
+  // Djkero_01,
+  // Varda_01,
+  // Carolyn_01,
+  // Yazid_01,
+  // Waadt_02,
+  // Safa_01,
   Acidmanhattan_01,
   Acidmanhattan_02,
   AfrizalRizki_01,
   Alterebro_01,
   Alterebro_02,
   Alterebro_03,
+  Annekatrinenderlein_01,
+  Burkabayram_01,
+  CRYPTEMES_01,
+  Imnot_01,
+  Itzcoatl_01,
+  Itzcoatl_02,
+  Joerres_01,
+  Linkous_03,
+  Maes_02,
+  Mumuthestan_02,
+  Mumuthestan_03,
+  Natived_01,
+  Norulesjustfeels_07,
+  OneLab_01,
+  OneLab_02,
+  Oviy_01,
+  Oviy_02,
+  Pilhei_01,
+  Pretorius_01,
+  Qizzio_02,
+  Rodicqart_01,
+  Three_01,
+  Varda_02,
+  Virgini_01,
+  Waadt_03,
+  Wans_01,
+  //-- SVG Convertsion issues
+  // Norulesjustfeels_05,
+  // Varda_03,
 ]
 
 // SVGs Prepared for ref (instancing icons from one source)
