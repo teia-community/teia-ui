aaea30cdbccbd8cadb472cc773f5fbc6c7f4b619
KikiPerryCT <kikiyuen05@yahoo.com.sg>
2024-05-05 22:36:45 +0800

    Add comments and refactor thumbnail generation a bit

--- src/utils/mint.ts
+++ src/utils/mint.ts
@@ -120,71 +120,110 @@ export const generateTypedArtCoverImage = async (
   txt: string,
   monospace: boolean
 ) => {
-  
-  const font = monospace ? 'Iosevka' : 'Source Sans Pro'
-  const cv_font = `16px ${font}`
-  if (txt.length === 0) {
-    return false
-  }
-  const createContext = function (width: number, height: number) {
-    const canvas = document.createElement('canvas')
-    canvas.width = width
-    canvas.height = height
-    const ctx = canvas.getContext('2d')
-    if (!ctx) throw new Error('Could not create canvas context')
-    return ctx
-  }
-  const c = createContext(512, 512)
-  c.filter = 'grayscale(100%)'
 
-  let dize = []
-  const lines = txt.split('\n')
-  dize = lines
-  c.font = cv_font
-  const x = 0
-  const y = 16
-  const lineheight = 16
-  const result = dize.reduce(
-    (r, e) => (c.measureText(r).width < c.measureText(e).width ? e : r),
-    ''
-  )
-  if (result.length > 130) {
-    c.canvas.width = 512
-    c.canvas.height = 512
-  } else {
-    c.canvas.width = c.measureText(result).width
-    c.canvas.height = lineheight * dize.length + 12
+  const font = monospace ? 'Iosevka' : 'Source Sans Pro';
+  const cv_font = `16px ${font}`;
+
+  if (txt.length === 0) {
+    return false;
   }
-  c.fillStyle = 'transparent'
-  c.font = cv_font
-  c.fillRect(0, 0, c.canvas.width, c.canvas.height)
-  c.filter = 'grayscale(100%)'
-  c.fillStyle = 'white'
-  if (c.canvas.width === 512) {
-    const s = lines[0].substring(0, 20)
-    c.fillText(s + '...', 16, 256)
+
+  // Function to create a 2D canvas context with specified width and height
+  const createContext = (width: number, height: number) => {
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas.getContext('2d');
+    if (!ctx) throw new Error('Could not create canvas context');
+    return ctx;
+  };
+
+  // Function to calculate canvas size based on input 
+  const calculateCanvasSize = (ctx, textLines: string[]) => {
+    const lineheight = 16;
+    const longestLine = textLines.reduce(
+      (longest, current) =>
+        ctx.measureText(longest).width < ctx.measureText(current).width
+          ? current
+          : longest,
+      ''
+    );
+
+    return longestLine.length > 130
+      ? { width: 512, height: 512 }
+      : { width: ctx.measureText(longestLine).width, height: lineheight * textLines.length + 12 };
+  };
+
+  // Create canvas context for drawing text
+  const c = createContext(512, 512);
+  c.filter = 'grayscale(100%)';
+  c.font = cv_font;
+
+  // Split text input into lines
+  const lines = txt.split('\n');
+  // Update canvas size based on text lines
+  const { width, height } = calculateCanvasSize(c, lines);
+
+  c.canvas.width = width;
+  c.canvas.height = height;
+
+  // Fill canvas with transparent color
+  // And set text color to white
+  c.fillStyle = 'transparent';
+  c.fillRect(0, 0, c.canvas.width, c.canvas.height);
+  c.filter = 'grayscale(100%)';
+  c.fillStyle = 'white';
+  c.font = cv_font;
+
+
+  // Draw text on canvas
+  // If text is longer than 512px, then only display first 20 char with ellipsis
+  if (width === 512) {
+    const truncatedText = lines[0].substring(0, 50) + '...';
+    c.fillText(truncatedText, 16, 256);
   } else {
-    for (let i = 0; i < lines.length; i++) {
-      c.fillText(lines[i], x, y + i * lineheight)
-    }
+    // Text does not exceed canvas width, so 
+    // Draw the text within the canvas
+    lines.forEach((line, i) => {
+      const x = 0;
+      const y = 16 + i * 16;
+      c.fillText(line, x, y);
+    });
   }
-  const ca = createContext(512, 512)
-  ca.fillStyle = 'transparent'
-  ca.fillRect(0, 0, 512, 512)
-  const cerceve = createContext(512, 512)
-  const cImage = new Image()
-  const img = new Image()
-  return new Promise((resolve, reject) => {
-    img.src = c.canvas.toDataURL('svg')
+
+  // Create auxillary canvas to perform image scaling of the text canvas
+  const ca = createContext(512, 512);
+  ca.fillStyle = 'transparent';
+  ca.fillRect(0, 0, 512, 512);
+  ca.font = cv_font
+
+  const cerceve = createContext(512, 512);
+  const cImage = new Image();
+  const img = new Image();
+
+  // Return a Promise that resolves to a File object
+  return new Promise<File>((resolve, reject) => {
+
+    // convert text canvas to image
+    img.src = c.canvas.toDataURL('svg');
+
     img.onload = function () {
-      ca.imageSmoothingEnabled = true
-      ca.width = 512
-      ca.height = 512
-      const hRatio = ca.width / img.width
-      const vRatio = ca.height / img.height
-      const ratio = Math.min(hRatio, vRatio)
-      const centerShift_x = (ca.width - img.width * ratio) / 2
-      const centerShift_y = (ca.height - img.height * ratio) / 2
+      // Configure smoothing for the auxillary canvas
+      ca.imageSmoothingEnabled = true;
+      ca.width = 512;
+      ca.height = 512;
+
+      // Calculate scaling ratio for the text image
+      const hRatio = ca.width / img.width;
+      const vRatio = ca.height / img.height;
+      const ratio = Math.min(hRatio, vRatio);
+
+      // Calculate centering offset when scaled image
+      // is placed in auxillary canvas
+      const centerShift_x = (ca.width - img.width * ratio) / 2;
+      const centerShift_y = (ca.height - img.height * ratio) / 2;
+
+      // Draw the scaled image on the auxillary canvas
       ca.drawImage(
         img,
         0,
@@ -195,25 +234,35 @@ export const generateTypedArtCoverImage = async (
         centerShift_y,
         img.width * ratio,
         img.height * ratio
-      )
-      const t = ca.canvas.toDataURL('svg')
-      cImage.src = t
-      cImage.onload = async function () {
-        cerceve.fillStyle = 'transparent'
-        cerceve.fillRect(0, 0, 512, 512)
-        cerceve.drawImage(cImage, 0, 0, 512, 512, 16, 16, 512 - 16, 512 - 16)
-        const ctx = cerceve.canvas.toDataURL('svg')
-        const res = await fetch(ctx)
-        const blob = await res.blob()
-        const file = new File([blob], 'Generated Cover.png', {
-          type: 'image/png',
-        })
+      );
 
-        resolve(file)
-      }
-    }
-  })
-}
+      // Convert auxillary canvas (with text image now) to data URL
+      const t = ca.canvas.toDataURL('svg');
+      cImage.src = t;
+
+      cImage.onload = async function () {
+       
+        // Clear the frame canvas
+        cerceve.fillStyle = 'transparent';
+        cerceve.fillRect(0, 0, 512, 512);
+
+        // Draw the scaled image on the frame canvas with proper padding
+        cerceve.drawImage(cImage, 0, 0, 512, 512, 16, 16, 512 - 32, 512 - 32);
+
+        // Convert frame canvas to data URL
+        const ctx = cerceve.canvas.toDataURL('svg');
+
+        // Fetch the data URL as a blob
+        const res = await fetch(ctx);
+        const blob = await res.blob();
+        const file = new File([blob], 'Generated Cover.png', { type: 'image/png' });
+
+        resolve(file);
+      };
+    };
+  });
+};
+
 
 export const convertFileToFileForm = async (
   fileToConvert: File
