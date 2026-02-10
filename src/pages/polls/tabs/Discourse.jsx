5cd08c98fec763ef4def59af5f9972f56fec2eee
joe simon <johannessimon@outlook.de>
2026-02-10 16:10:36 +0100

    Removed more unused code

--- src/pages/polls/tabs/Discourse.jsx
+++ src/pages/polls/tabs/Discourse.jsx
@@ -1,7 +1,5 @@
 import styles from '@style'
 
-const DISCOURSE_CATEGORY_URL = 'https://discourse.teia.art/c/teia-dao/12'
-
 export default function Discourse() {
   return (
     <section className={styles.section}>
