aa9f199d19b87cf1a3389c1bd82d90cf98116bdf
joe simon <johannessimon@outlook.de>
2026-04-16 10:19:36 +0200

    Add Shadownet Info Banner

--- src/pages/messages/index.jsx
+++ src/pages/messages/index.jsx
@@ -2,7 +2,11 @@ import { Link } from 'react-router-dom'
 import { Page } from '@atoms/layout'
 import { Button } from '@atoms/button'
 import { useUserStore } from '@context/userStore'
+import { MESSAGING_TZKT_API } from '@constants'
 import styles from './index.module.scss'
+
+const SHADOWNET_RPC = 'https://rpc.shadownet.teztnets.com'
+const SHADOWNET_TZKT = MESSAGING_TZKT_API
 
 export default function MessagesInbox() {
   const address = useUserStore((st) => st.address)
@@ -24,6 +28,31 @@ export default function MessagesInbox() {
     <Page title="Messages">
       <div className={styles.container}>
         <h2 className={styles.headline}>Messages</h2>
+
+        <div className={styles.info}>
+          <p>
+            This is an isolated testing environment connected to the Tezos
+            Shadownet.
+          </p>
+          <p>Contracts deployed here use test tez and do not affect mainnet.</p>
+          <p>
+            <strong>Get shadownet tez:</strong>{' '}
+            <a
+              href="https://faucet.shadownet.teztnets.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.faucetLink}
+            >
+              https://faucet.shadownet.teztnets.com/
+            </a>
+          </p>
+          <p>
+            <strong>RPC:</strong> <code>{SHADOWNET_RPC}</code>
+          </p>
+          <p>
+            <strong>TzKT:</strong> <code>{SHADOWNET_TZKT}</code>
+          </p>
+        </div>
 
         <div className={styles.sections}>
           <Link to="/messages/dm" className={styles.sectionCard}>
